2c5270d208a93cd545293aea7281b3794057d08f
GajiPH13 <islamgajinajrul@gmail.com>
2026-04-28 22:46:04 +0200

    URL fixed in auth-client.js

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -1,7 +1,7 @@
 import { createAuthClient } from "better-auth/react"
 export const authClient = createAuthClient({
     /** The base URL of the server (optional if you're using the same domain) */
-    baseURL: "http://localhost:3000"
+    baseURL: "https://ai-pic-generator-livid.vercel.app",
 })
 // const signIn =async () => {
 //     const data = await authClient.signIn.social({
