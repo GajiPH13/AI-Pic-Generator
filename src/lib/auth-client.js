54ab6051678ac512795e4962ec564e368b923ce2
GajiPH13 <islamgajinajrul@gmail.com>
2026-04-29 22:24:54 +0200

    Duplicate hardCode Fix

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -3,11 +3,11 @@ export const authClient = createAuthClient({
     /** The base URL of the server (optional if you're using the same domain) */
       // baseURL: "https://ai-pic-generator-livid.vercel.app"
     //  baseURL: "http://localhost:3000"
-    baseURL: " ",
+    baseURL: " /",
 })
 // const signIn =async () => {
 //     const data = await authClient.signIn.social({
 //         provider:"google",
 
 // })
-export const { signIn, signUp, useSession } = createAuthClient()
+export const { signIn, signUp, useSession,signOut } = authClient()
--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,63 +1,23 @@
-// import { betterAuth } from "better-auth";
-// import { MongoClient } from "mongodb";
-// import { mongodbAdapter } from "better-auth/adapters/mongodb";
-
-// const client = new MongoClient(process.env.MONGODB_URL);
-// const db = client.db("pixgen");
-
-// export const auth = betterAuth({
-//   database: mongodbAdapter(db, {
-//     // Optional: if you don't provide a client, database transactions won't be enabled.
-//     client
-//   }),
-//   emailAndPassword: { 
-//     enabled: true, 
-//   }, 
-//   socialProviders: {
-//     google: {
-//         prompt: "select_account", 
-//         clientId: process.env.GOOGLE_CLIENT_ID,
-//         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-//     },
-// }
-// });
 import { betterAuth } from "better-auth";
 import { MongoClient } from "mongodb";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
 
-const uri = process.env.MONGODB_URL;
-
-if (!uri) {
-  throw new Error("Missing MONGODB_URL");
-}
-
-let client;
-let clientPromise;
-
-if (!global._mongoClientPromise) {
-  client = new MongoClient(uri);
-  global._mongoClientPromise = client.connect();
-}
-
-clientPromise = global._mongoClientPromise;
-
-const connectedClient = await clientPromise;
-const db = connectedClient.db("pixgen");
+const client = new MongoClient(process.env.MONGODB_URL);
+const db = client.db("pixgen");
 
 export const auth = betterAuth({
   database: mongodbAdapter(db, {
-    client: connectedClient,
+    // Optional: if you don't provide a client, database transactions won't be enabled.
+    client
   }),
-
-  emailAndPassword: {
-    enabled: true,
-  },
-
+  emailAndPassword: { 
+    enabled: true, 
+  }, 
   socialProviders: {
     google: {
-      prompt: "select_account",
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        prompt: "select_account", 
+        clientId: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     },
-  },
+}
 });
