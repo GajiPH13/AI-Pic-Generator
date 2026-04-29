434f8bb4e95ce145d5cd36d61fe57825d24b6578
GajiPH13 <islamgajinajrul@gmail.com>
2026-04-29 22:36:59 +0200

    Error

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -1,13 +1,26 @@
-import { createAuthClient } from "better-auth/react"
-export const authClient = createAuthClient({
-    /** The base URL of the server (optional if you're using the same domain) */
-      // baseURL: "https://ai-pic-generator-livid.vercel.app"
-    //  baseURL: "http://localhost:3000"
-    baseURL: " /",
-})
-// const signIn =async () => {
-//     const data = await authClient.signIn.social({
-//         provider:"google",
-
+// import { createAuthClient } from "better-auth/react"
+// export const authClient = createAuthClient({
+//     /** The base URL of the server (optional if you're using the same domain) */
+//       // baseURL: "https://ai-pic-generator-livid.vercel.app"
+//     //  baseURL: "http://localhost:3000"
+//     baseURL: " /",
 // })
-export const { signIn, signUp, useSession,signOut } = authClient()
+// // const signIn =async () => {
+// //     const data = await authClient.signIn.social({
+// //         provider:"google",
+
+// // })
+// export const { signIn, signUp, useSession,signOut } = authClient()
+
+"use client";
+
+import { createAuthClient } from "better-auth/react";
+
+export const authClient = createAuthClient();
+
+export const {
+  signIn,
+  signOut,
+  signUp,
+  useSession,
+} = authClient;
